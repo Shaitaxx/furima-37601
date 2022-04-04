8f8ae8f44656f070fd0f7fc1d76c5e80b7ed74e9
Shaitaxx <y.kenta.x@icloud.com>
2022-04-04 13:32:22 +0900

    バリデーションの設定

--- app/models/good.rb
+++ app/models/good.rb
@@ -4,4 +4,14 @@ class Good < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :price, presence: true
 end
