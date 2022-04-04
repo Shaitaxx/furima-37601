d528eeb742d8d8da795152be97d061b570d71385
Shaitaxx <y.kenta.x@icloud.com>
2022-04-04 22:37:03 +0900

    商品出品ページの作成

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,6 +1,10 @@
 class Good < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :region
+  belongs_to :shipping_date
 
   belongs_to :user
   has_one_attached :image
@@ -8,10 +12,11 @@ class Good < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :shipping_date_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :price, presence: true
+  validates :user, presence: true
 end
