3944c84ae73fcff939509ca82fa455ba8aaf2bb0
Shaitaxx <y.kenta.x@icloud.com>
2022-04-06 18:42:29 +0900

    修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,7 +2,7 @@ class TopsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @goods = Good.all
+    @goods = Good.all.order("created_at DESC")
   end
 
   def new
